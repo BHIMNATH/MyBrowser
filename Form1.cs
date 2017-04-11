d97f65070a91092cfa863aaed6b7f0fba81d9b48
BHIMNATH <you@example.com>
2017-04-11 20:41:17 +0530

    A little Change

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,47 @@ namespace Brow
             InitializeComponent();
         }
 
-  
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s=(ty.Text);
+            web.Navigate(s);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            if (web.CanGoBack)
+                web.GoBack();
+        }
+
+        private void front_Click(object sender, EventArgs e)
+        {
+            if (web.CanGoForward)
+                web.GoForward();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+                web.Refresh();
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
